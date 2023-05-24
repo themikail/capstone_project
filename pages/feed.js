8a228a235a5f14a567c68925520a6b6eaba3b88d
themikail <hello@mikailgokce.com>
2023-05-24 10:02:32 +0200

    change cards.js

---
+++ pages/feed.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Cards from "./components/cards";
+
+export default function Feed() {
+  return (
+    <h1>
+      <Cards />
+    </h1>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Feed from "./feed";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <Feed />
     </div>
   );
 }
