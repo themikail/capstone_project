8d468a2ab0b93dac9c8b6236d591ea85afd8d5d3
themikail <hello@mikailgokce.com>
2023-05-26 22:22:42 +0200

    .

--- pages/components/feed.js
+++ pages/components/feed.js
@@ -1,13 +1,11 @@
 import Cards from "./cards";
 import ActiveMusic from "./activeMusic";
-import Navbar from "./navBar";
 
 export default function Feed() {
   return (
     <div>
       <ActiveMusic />
       <Cards />
-      <Navbar />
     </div>
   );
 }
