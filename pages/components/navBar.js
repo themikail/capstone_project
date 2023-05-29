b81242d558109fcc51de5f1a45026003de6466c6
themikail <hello@mikailgokce.com>
2023-05-29 13:03:22 +0200

    .

--- pages/components/navBar.js
+++ pages/components/navBar.js
@@ -1,1 +1,25 @@
-git push --set-upstream origin Navbar
+import React from "react";
+import styled from "styled-components";
+
+export default function Navbar() {
+  return (
+    <NavbarContainer>
+      <NavbarButton icon={RiDiscLine} />
+      <NavbarButton icon={RiAddCircleLine} />
+      <NavbarButton icon={RiUserLine} />
+    </NavbarContainer>
+  );
+}
+
+const NavbarContainer = styled.nav`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 60px;
+  background-color: #f1f1f1;
+  display: flex;
+  justify-content: space-around;
+  padding: 10px;
+  z-index: 999;
+`;
