88a21568459cd23cfd3e7f8fd9b5dfe5f31b73b9
themikail <hello@mikailgokce.com>
2023-05-31 12:24:23 +0200

    change

--- pages/components/post.js
+++ pages/components/post.js
@@ -1,12 +1,42 @@
 import React from "react";
-import ActiveMusic from "./activeMusic";
-export default function Post() {
+import Image from "next/image";
+import styled from "styled-components";
+
+export default function Post({ songName, coverPhotoSrc }) {
   return (
     <>
       <h2>Modal Content</h2>
-      <p>This is the content of the modal.</p>
-      <ActiveMusic />
-      <button>Post</button>
+      <div>
+        <SongInfo>
+          <SongCover
+            src={coverPhotoSrc}
+            alt="Song Cover"
+            width={200}
+            height={200}
+          />
+          <SongDetails>
+            <SongName>{songName}</SongName>
+          </SongDetails>
+        </SongInfo>
+      </div>
     </>
   );
 }
+
+const SongInfo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SongCover = styled(Image)`
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+const SongDetails = styled.div`
+  margin-left: 20px;
+`;
+
+const SongName = styled.h3`
+  margin: 0;
+`;
