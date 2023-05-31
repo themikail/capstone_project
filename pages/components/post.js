e01cf21796c18acf478de57b0c9b6b214669809e
themikail <hello@mikailgokce.com>
2023-05-31 11:56:13 +0200

    add file

--- pages/components/post.js
+++ pages/components/post.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import ActiveMusic from "./activeMusic";
 export default function Post() {
   return (
     <>
       <h2>Modal Content</h2>
       <p>This is the content of the modal.</p>
+      <ActiveMusic />
       <button>Post</button>
     </>
   );
