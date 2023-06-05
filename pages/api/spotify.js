78e4bb933c0d3d2bfe9c52c8061ec73b1c9ac944
themikail <hello@mikailgokce.com>
2023-06-05 10:49:11 +0200

    switch api from frontend to backend

---
+++ pages/api/spotify.js
@@ -0,0 +1,38 @@
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotifyApi = new SpotifyWebApi();
+
+export function loginWithSpotify() {
+  const clientId = "a7cfeef84deb42289fa131b3fe9babc2";
+  const redirectUri = "http://localhost:3000";
+
+  const scopes = ["user-read-private", "user-read-email"];
+
+  const authorizeUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+  )}&response_type=token`;
+
+  window.location.href = authorizeUrl;
+}
+
+export function handleSpotifyCallback() {
+  const params = new URLSearchParams(window.location.hash.substr(1));
+  const accessToken = params.get("access_token");
+  const expiresIn = params.get("expires_in");
+
+  if (accessToken) {
+    // Speichere den Zugriffstoken und andere Informationen im Zustand oder der lokalen Speicherung
+
+    // Setze den Zugriffstoken für die Spotify-Web-API-Instanz
+    spotifyApi.setAccessToken(accessToken);
+
+    // Führe Anfragen an die Spotify API mit dem zugewiesenen Zugriffstoken durch
+    spotifyApi.getMe().then((response) => {
+      console.log(response);
+      router.push("/components/feed"); // Redirect to feed page
+    });
+  } else {
+    // Handle authentication failure
+    console.log("Authentication failed");
+  }
+}
--- pages/components/login.js
+++ pages/components/login.js
@@ -3,49 +3,12 @@ import Image from "next/image";
 import Link from "next/link";
 import styled, { createGlobalStyle } from "styled-components";
 import { useRouter } from "next/router";
-import SpotifyWebApi from "spotify-web-api-js";
-
-const spotifyApi = new SpotifyWebApi();
+import { loginWithSpotify, handleSpotifyCallback } from "../api/spotify";
 
 export default function Login() {
   const router = useRouter();
 
-  const handleLogin = () => {
-    const clientId = "a7cfeef84deb42289fa131b3fe9babc2";
-    const redirectUri = "http://localhost:3000";
-
-    const scopes = ["user-read-private", "user-read-email"];
-
-    const authorizeUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-      "%20"
-    )}&response_type=token`;
-
-    window.location.href = authorizeUrl;
-  };
-
   useEffect(() => {
-    const handleSpotifyCallback = () => {
-      const params = new URLSearchParams(window.location.hash.substr(1));
-      const accessToken = params.get("access_token");
-      const expiresIn = params.get("expires_in");
-
-      if (accessToken) {
-        // Speichere den Zugriffstoken und andere Informationen im Zustand oder der lokalen Speicherung
-
-        // Setze den Zugriffstoken für die Spotify-Web-API-Instanz
-        spotifyApi.setAccessToken(accessToken);
-
-        // Führe Anfragen an die Spotify API mit dem zugewiesenen Zugriffstoken durch
-        spotifyApi.getMe().then((response) => {
-          console.log(response);
-          router.push("/components/feed"); // Redirect to feed page
-        });
-      } else {
-        // Handle authentication failure
-        console.log("Authentication failed");
-      }
-    };
-
     // Überprüfe, ob der Zugriffstoken in der URL vorhanden ist
     if (window.location.hash.includes("access_token")) {
       handleSpotifyCallback();
@@ -58,7 +21,7 @@ export default function Login() {
         <Title>
           <h1>Share your lovely music with Musicc</h1>
         </Title>
-        <StyledButton onClick={handleLogin}>
+        <StyledButton onClick={loginWithSpotify}>
           <Link href="/components/feed">
             <ButtonText>Login with Spotify</ButtonText>
           </Link>
